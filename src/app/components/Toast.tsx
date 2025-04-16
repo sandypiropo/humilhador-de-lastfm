22f5f3d335492981eb6ef9dc9724b8365096ea6f
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2025-04-15 21:06:15 -0300

    Mudando toast de lugar

--- src/app/components/Toast.tsx
+++ src/app/components/Toast.tsx
@@ -4,15 +4,18 @@ import { useEffect, useState } from 'react';
 interface ToastProps {
   message: string;
   show: boolean;
-  duration?: number; 
+  duration?: number;
   onClose: () => void;
 }
 
 export default function Toast({ message, show, duration = 5000, onClose }: ToastProps) {
+  const [showToast, setShowToast] = useState(show);
 
   useEffect(() => {
     if (show) {
+      setShowToast(true);
       const timer = setTimeout(() => {
+        setShowToast(false);
         onClose();
       }, duration);
 
@@ -20,14 +23,15 @@ export default function Toast({ message, show, duration = 5000, onClose }: Toast
     }
   }, [show, duration, onClose]);
 
-  if (!show) return null;
-
   return (
-    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-[#E6E6FA] text-black px-6 py-4 rounded-md shadow-lg animate-fadeInUp max-w-xs sm:max-w-lg w-full">
-      <div className="flex items-center justify-center gap-2 text-center">
-        <p className="text-base sm:text-lg font-semibold text-center break-words">{message}</p>
+    showToast ? (
+      <div className={`fixed ${showToast ? 'animate-fadeInUp' : 'animate-fadeOutDown'} max-w-xs sm:max-w-lg w-full
+        top-6 right-6 bg-[#E6E6FA] text-black px-6 py-4 rounded-md shadow-lg`}>
+        <div className="flex items-center justify-center gap-2 text-center">
+          <p className="text-base sm:text-lg font-semibold text-center break-words">{message}</p>
+        </div>
+        <span role="img" aria-label="heart" className="text-2xl">❤️</span>
       </div>
-      <span role="img" aria-label="heart" className="text-2xl">❤️</span>
-    </div>
+    ) : null
   );
 }
