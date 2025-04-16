99d43d73df5d4e5d37148787de325076f49dbc12
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2025-04-15 23:00:53 -0300

    Atualizando eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Nunito } from 'next/font/google'
 
 const nunito = Nunito({ subsets: ['latin'], weight: ['400', '700'] })
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
